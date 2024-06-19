9f0f4ecdd5c9979957be2d13d8cef48b8c25457e
erikprakoso <erikprakoso1993@gmail.com>
2024-06-19 19:58:53 +0700

    chore: update library

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from config import Config
 from routes import ml_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
 app.register_blueprint(ml_bp, url_prefix='/api/ml')
 
